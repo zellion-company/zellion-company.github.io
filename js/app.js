46c3457d4231abf10c50efd90d30a33a85cda89c
Michael Disko <disko@art-lemon.com>
2017-02-27 12:00:40 +0200

    new

--- js/app.js
+++ js/app.js
@@ -58,12 +58,30 @@
 
 
 	$(function () {
+	  $(window).on('load scroll', function () {
+	    if ($(this).scrollTop() > 0) {
+	      $('.header').addClass('active');
+	    } else {
+	      $('.header').removeClass('active');
+	    }
+	  });
 	  (0, _navigation2.default)();
 	  $('#section-1').zellionTabs();
 	  $('#section-2').zellionTabs({
-	    defaultTab: 4
+	    animation: 'fade'
 	  });
 	  $('#section-3').zellionTabs({
+	    animation: 'slide',
+	    direction: 'left'
+	  });
+	  $('#section-4').zellionTabs({
+	    animation: 'slide',
+	    direction: 'bottom'
+	  });
+	  $('#section-5').zellionTabs({
+	    defaultTab: 4
+	  });
+	  $('#section-6').zellionTabs({
 	    autoplay: true
 	  });
 	});
